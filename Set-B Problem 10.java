7dffe7cd4b270c1c1358640a481cd9ed21e20247
fahim15hub <muntasirf15@gmail.com>
2025-09-13 11:59:30 +0600

    Create Set-B Problem 10.java

---
+++ Set-B Problem 10.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main
+{
+	
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		String arr = sc.nextLine();
+		System.out.println(arr.toUpperCase());
+		System.out.println(arr.toLowerCase());
+	}
+}
